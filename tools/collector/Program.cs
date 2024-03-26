c1a775ce08cc09cf2ddc1fb778e2e6aa8156cfbf
Pocol <project.asura@gmail.com>
2024-03-27 01:38:40 +0900

    開発環境構築。

--- tools/collector/Program.cs
+++ tools/collector/Program.cs
@@ -8,56 +8,86 @@ namespace collector
     {
         static void Main(string[] args)
         {
-            if (args.Length < 2)
+            if (args.Length < 3)
             {
-                Console.WriteLine("collector.exe {rootPath} {exeName} {header}");
-                Console.WriteLine("[rootPath] : ソースコードのルートディレクトリを指定します. (ex) C:\\root\\test");
-                Console.WriteLine("[exeName]  : 出力するバイナリ名を指定します. (ex) C:\\root\\test\\bin\\game.gb");
-                Console.WriteLine("[header]   : ファイル先頭に付加する文字列を指定します.");
+                Console.WriteLine("collector.exe inputDir outDir binName (ruleNinja)");
+                Console.WriteLine("[inputPath] : ソースコードのルートディレクトリを指定します. (ex) C:\\root\\test");
+                Console.WriteLine("[outputDir] : 出力ディレクトリです.");
+                Console.WriteLine("[binName]   : 出力するバイナリ名を指定します.フォルダパスは含めないで下さい. (ex) game.gb");
+                Console.WriteLine("[ruleNinja] : (オプション) ファイル先頭に付加する文字列を指定します.");
                 return;
             }
 
-            var rootPath = args[0];
-            var output   = args[1];
-            var header   = (args.Length >= 3) ? args[2] : null;
+            var rootPath = Path.GetFullPath(args[0]);
+            var outPath  = Path.GetFullPath(args[1]);
+            var binName  = CorrectPath(Path.Combine(outPath, args[2]));
+            var rulePath = (args.Length >= 4) ? args[3] : null;
 
             if (!Directory.Exists(rootPath))
             {
-                Console.Error.WriteLine($"Root Path Not Found. Path={rootPath}");
+                Console.Error.WriteLine($"Root Path Not Found. Path=\"{rootPath}\".");
                 return;
+            }
+
+            if (!Directory.Exists(outPath))
+            {
+                Directory.CreateDirectory(outPath);
             }
 
             var files = Directory.GetFiles(rootPath, "*.c", SearchOption.AllDirectories);
 
+            if (files.Length == 0)
+            {
+                Console.Error.WriteLine("C File Not Found.");
+                return;
+            }
+
             var sources = new StringBuilder();
             var builder = new StringBuilder();
 
-            if (header != null)
+            if (!string.IsNullOrEmpty(rulePath) && File.Exists(rulePath))
             {
-                builder.Append(header);
-                builder.Append("\n");
+                using (var reader = new StreamReader(rulePath))
+                {
+                    string line;
+                    while((line = reader.ReadLine() ) != null)
+                    {
+                        builder.Append(line);
+                        builder.Append("\n");
+                    }
+                }
             }
 
             builder.Append($"# Compile {files.Length} Files.\n");
             foreach (var sourceName in files)
             {
-                var srcName = sourceName.ToLower();
-                srcName = srcName.Replace("c:\\", "c$:\\");
-                var objName = Path.ChangeExtension(srcName, ".o");
+                var srcName = CorrectPath(sourceName);
+                var objName = Path.GetFullPath(sourceName);
+                objName = objName.Replace(rootPath + "\\", outPath + "\\");
+                objName = Path.ChangeExtension(objName, ".o");
+                objName = CorrectPath(objName);
                 sources.Append(objName + " ");
                 builder.Append($"build {objName}: compile {srcName}\n");
             }
             builder.Append("# Link\n");
-            builder.Append($"build {output}: link {sources}\n");
+            builder.Append($"build {binName}: link {sources}\n");
 
-            var listName = Path.Combine(rootPath, "collected.ninja");
+            var listName = Path.Combine(outPath, "build.ninja");
             using (var stream = File.Create(listName))
-            using (var writer = new StreamWriter(stream, Encoding.UTF8))
+            using (var writer = new StreamWriter(stream, Encoding.ASCII))
             {
                 writer.Write(builder.ToString());
             }
 
-            Console.WriteLine($"File List Output! Path ={listName}");
+            Console.WriteLine($"Generate File List. Path =\"{listName}\".");
+        }
+
+        static string CorrectPath(string path)
+        {
+            var p = Path.GetFullPath(path);
+            p = p.ToLower();
+            p = p.Replace(":\\", "$:\\");
+            return p;
         }
     }
 }
