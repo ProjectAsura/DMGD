6e21e3d775f642e3c88656be867ebc1120ce1e6f
Pocol <project.asura@gmail.com>
2024-03-31 12:16:13 +0900

    ROM焼きツールのアドレスを追加。

---
+++ runtime/app/include/typedef.h
@@ -0,0 +1,18 @@
+#ifndef TYPEDEF_H
+#define TYPEDEF_H
+
+/*---------------------------------------------------------------------------*/
+/*  Includes                                                                 */
+/*---------------------------------------------------------------------------*/
+#include <stdint.h>
+#include <stdbool.h>
+#include <assert.h>
+
+typedef signed char    s8;
+typedef unsigned char  u8;
+typedef signed short   s16;
+typedef unsigned short u16;
+typedef signed int     s32;
+typedef unsigned int   u32;
+
+#endif/*TYPEDEF_H*/
